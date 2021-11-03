29ff22b8eb213c8c640d52e68a3aa44b5bc1a50e
Emir Turkes <eturkes@bu.edu>
2021-11-03 10:48:01 +0000

    Add R analysis boilerplate

--- R/run_all.R
+++ R/run_all.R
@@ -1,22 +1,23 @@
-# Copyright 2021 Emir Turkes, UK DRI at UCL
-# 
+# Copyright 2021 Emir Turkes, Sebastiaan De Schepper, UK DRI at UCL
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-FROM eturkes/r-docker-minimal:R4.1.1v1
+# This script runs all files in the analysis.
+# Sections can be commented out as needed.
 
-RUN Rscript -e "install.packages('rmarkdown')" \
-        -e "install.packages('mime')" \
-    && rm -Rf /tmp/downloaded_packages/ \
-        /tmp/*.rds
+setwd(dirname(parent.frame(2)$ofile)) # Move to location of this file.
 
-LABEL maintainer="Emir Turkes emir.turkes@eturkes.com"
+rmarkdown::render(
+  file.path("01_SPP1_expression.Rmd"),
+  output_file = file.path("..", "results", "01-SPP1-expression")
+)
